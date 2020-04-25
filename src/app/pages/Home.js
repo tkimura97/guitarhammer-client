e257b5dbc22d9af2953c259cd634dfc7ff19edc6
tkimura97 <taigakimura97@gmail.com>
2020-04-25 03:01:01 -0700

    added chord generator

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -1,5 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Chord from "@tombatossals/react-chords/lib/Chord";
+
+const MyChord = () => {
+  const chord = {
+    frets: [1, 3, 3, 2, 1, 1],
+    fingers: [1, 3, 4, 2, 1, 1],
+    barres: [1],
+    capo: false,
+  };
+  const instrument = {
+    strings: 6,
+    fretsOnChord: 4,
+    name: "Guitar",
+    keys: [],
+    tunings: {
+      standard: ["E", "A", "D", "G", "B", "E"],
+    },
+  };
+  const lite = false; // defaults to false if omitted
+  return <Chord chord={chord} instrument={instrument} lite={lite} />;
+};
 
 class Home extends Component {
   render() {
@@ -11,6 +32,7 @@ class Home extends Component {
         <Link to="/search">
           <li>Search</li>
         </Link>
+        <MyChord />
       </div>
     );
   }
